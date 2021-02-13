b50658ad718aea95bba2ee7aca44d507ffb61df1
Onor Bumbum <danabeyin@gmail.com>
2021-02-12 17:39:56 -0800

    2

--- harrow/settings.py
+++ harrow/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'honthrd8@_voycb3smn4q5$6svm_o@$-23__eji0vx(_$d#y8o'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'honthrd8@_voycb3smn4q5$6svm_o@$-23__eji0vx(_$d#y8o')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
